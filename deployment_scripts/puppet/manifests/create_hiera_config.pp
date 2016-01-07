7df2d121e764c45ca3bf3ca98fcbbd65a086d1d0
Aleksandr Didenko <adidenko@mirantis.com>
2016-01-07 17:12:38 +0100

    Fix hiera override file name
    It should be named exactly as plugin.

--- deployment_scripts/puppet/manifests/create_hiera_config.pp
+++ deployment_scripts/puppet/manifests/create_hiera_config.pp
@@ -3,7 +3,7 @@ notice('MODULAR: create_hiera_config.pp')
 
 $external_lb = hiera('external_lb')
 
-file {'/etc/hiera/plugins/fuel-plugin-external-lb.yaml':
+file {'/etc/hiera/plugins/external_lb.yaml':
   ensure  => file,
   content => inline_template("# Created by puppet, please do not edit manually
 network_metadata:
